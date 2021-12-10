b4497e03dd1fd0e62b5abe5beb20c6c808a4250f
karolcie <kisie_l@op.pl>
2021-12-10 10:41:59 +0100

    add new section HomeThreeColumns

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+
 const HomeThreeColumns = () => {
     return (
-        <div>
-            tu beda 3 kolumny
+        <div className="homeThreeColumnsBox">
+            <div className="homeThreeColumnsBox_Column"></div>
+                <div className="homeThreeColumnsBox__Number">10</div>
+                <div className="homeThreeColumnsBox__Title">ODDANYCH WORKÓW</div>
+                <div className="homeThreeColumnsBox__Text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
+            <div className="homeThreeColumnsBox_Column"></div>
+                <div className="homeThreeColumnsBox__Number">5</div>
+                <div className="homeThreeColumnsBox__Title">WSPARTYCH ORGANIZACJI</div>
+                <div className="homeThreeColumnsBox__Text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
+            <div className="homeThreeColumnsBox_Column"></div>
+                <div className="homeThreeColumnsBox__Number">7</div>
+                <div className="homeThreeColumnsBox__Title">ZORGANIZOWANY ZBIÓREK</div>
+                <div className="homeThreeColumnsBox__Text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
         </div>
     );
 };
