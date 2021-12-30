b365bdc6df118fb1c307be2626b9f7fc3f5fe7bc
karolcie <kisie_l@op.pl>
2021-12-30 19:09:43 +0100

    add database to section WhoWeHelp

---
+++ src/database/data.js
@@ -0,0 +1,114 @@
+const data = {
+    organizations : {
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+            " sed do eiusmod tempor incididunt ut labore et " +
+            "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+        "items": [
+            {
+                "title": "Organizacja “Lorem Ipsum 1”",
+                "subtitle": "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+                "things": "Egestas, sed, tempus"
+            },
+            {
+                "title": "Organizacja “Lorem Ipsum 2”",
+                "subtitle": "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+                "things": "Ut, aliquam, purus, sit, amet"
+            },
+            {
+                "title": "Organizacja “Lorem Ipsum 3”",
+                "subtitle": "Scelerisque in dictum non consectetur a erat nam.",
+                "things": "Mi, quis, hendrerit, dolor"
+            },
+            {
+                "title": "Organizacja “Lorem Ipsum 4”",
+                "subtitle": "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+                "things": "Egestas, sed, tempus"
+            },
+            {
+                "title": "Organizacja “Lorem Ipsum 5”",
+                "subtitle": "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+                "things": "Ut, aliquam, purus, sit, amet"
+            },
+            {
+                "title": "Organizacja “Lorem Ipsum 6”",
+                "subtitle": "Scelerisque in dictum non consectetur a erat nam.",
+                "things": "Mi, quis, hendrerit, dolor"
+            },
+        ]
+    },
+    fund : {
+        "description": "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy." +
+            " Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+        "items": [
+            {
+                "title": "Fundacja “Dbam o Zdrowie”",
+                "subtitle": "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+                "things": "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+            },
+            {
+                "title": "Fundacja “Dla dzieci”",
+                "subtitle": "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+                "things": "ubrania, meble, zabawki"
+            },
+            {
+                "title": "Fundacja “Bez domu”",
+                "subtitle": "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+                "things": "ubrania, jedzenie, ciepłe koce"
+            },
+            {
+                "title": "Fundacja “Lorem Ipsum 1”",
+                "subtitle": "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+                "things": "ubrania, jedzenie, ciepłe koce"
+            },
+            {
+                "title": "Fundacja “Lorem Ipsum 2”",
+                "subtitle": "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+                "things": "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+            },
+            {
+                "title": "Fundacja “Lorem Ipsum 3”",
+                "subtitle": "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+                "things": "ubrania, meble, zabawki"
+            },
+            {
+                "title": "Fundacja “Lorem Ipsum 4”",
+                "subtitle": "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",
+                "things": "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+            },
+            {
+                "title": "Fundacja “Lorem Ipsum 5”",
+                "subtitle": "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+                "things": "ubrania, meble, zabawki"
+            },
+            {
+                "title": "Fundacja “Lorem Ipsum 6”",
+                "subtitle": "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.",
+                "things": "ubrania, jedzenie, ciepłe koce"
+            },
+        ]
+    },
+    local : {
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " +
+            "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
+        "items": [
+            {
+                "title": "Zbiórka “Lorem Ipsum 1”",
+                "subtitle": "Quis varius quam quisque id diam vel quam elementum pulvinar.",
+                "things": "Egestas, sed, tempus"
+            },
+            {
+                "title": "Zbiórka “Lorem Ipsum 2”",
+                "subtitle": "Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.",
+                "things": "Ut, aliquam, purus, sit, amet"
+            },
+            {
+                "title": "Zbiórka “Lorem Ipsum 3”",
+                "subtitle": "Scelerisque in dictum non consectetur a erat nam.",
+                "things": "Mi, quis, hendrerit, dolor"
+            },
+        ]
+    }
+};
+
+
+export default data;
