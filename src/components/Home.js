9510b3740abb3ea60b887d78018bb41c29109c88
karolcie <kisie_l@op.pl>
2022-01-03 12:40:45 +0100

    add components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import HomeHeader from "./HomeHeader";
 import HomeThreeColumns from "./HomeThreeColumns";
+import HomeSimpleSteps from "./HomeSimpleSteps";
 import HomeAbout from "./HomeAbout";
+import HomeWhoWeHelp from "./HomeWhoWeHelp";
+import HomeContact from "./HomeContact";
 
 const Home = () => {
     return (
         <>
             <HomeHeader />
             <HomeThreeColumns />
-
+            <HomeSimpleSteps />
             <HomeAbout />
+            <HomeWhoWeHelp />
+            <HomeContact />
         </>
     );
 };
